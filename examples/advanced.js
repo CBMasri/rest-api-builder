60055e2621e6561fe032aeb316e69391666ee9e2
Carl Masri <carl.masri@gmail.com>
2021-01-02 15:57:27 -0800

    Update advanced examples

--- examples/advanced.js
+++ examples/advanced.js
@@ -13,17 +13,23 @@ const MyAPI = {
   foo: builder.create({
     path: 'bar',
     endpoints: [
+      { action: 'list' },
       { action: 'retrieve' },
       { action: 'custom1', method: 'get', path: ':n1' },
-      { action: 'custom2', method: 'put', path: ':n1/baz/:n2' }
+      { action: 'custom2', method: 'put', path: ':n1/baz/:n2' },
+      { action: 'custom3', method: 'post', path: 'baz/:n1' },
+      { action: 'custom4', method: 'delete', path: 'baz/:n1' }
     ]
   })
 }
 
 console.log('MyAPI:', MyAPI)
 
-const params = { params: { q: 'example' } }
+const extraConfig = { params: { q: 'example' } }
 
-MyAPI.foo.retrieve(1, params)
-MyAPI.foo.custom1(1, params)
-MyAPI.foo.custom2({ n1: 1, n2: 2 }, null, params)
+MyAPI.foo.list(extraConfig)
+MyAPI.foo.retrieve(1, extraConfig)
+MyAPI.foo.custom1(1, extraConfig)
+MyAPI.foo.custom2({ n1: 1, n2: 2 }, null, extraConfig)
+MyAPI.foo.custom3(1, null, extraConfig)
+MyAPI.foo.custom4(1, extraConfig)
